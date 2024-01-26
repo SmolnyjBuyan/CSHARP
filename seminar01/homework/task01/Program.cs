77a89fffebf533d1a0a0f28d220f53840a30857d
SmolnyjBuyan <564kot9918@mail.ru>
2024-01-26 11:40:13 +0300

    add seminar1:homework:task1

---
+++ seminar01/homework/task01/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstNumber = 3;
+int secondNumber = 3;
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое число больше");
+}
+
+if (firstNumber < secondNumber)
+{
+    Console.WriteLine("второе число больше");
+}
+
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine("числа равны");
+}
