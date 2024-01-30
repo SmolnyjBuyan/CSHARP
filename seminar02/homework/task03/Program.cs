3e18c2256b244294c28263fc22f205150d47d594
SmolnyjBuyan <564kot9918@mail.ru>
2024-01-30 11:28:38 +0300

    add seminar02:homework:task03

---
+++ seminar02/homework/task03/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 40;
+if (number >= 10 && number <= 99)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int max;
+    if (firstDigit > secondDigit)
+    {
+        max = firstDigit;
+    }
+    else
+    {
+        max = secondDigit;
+    }
+    Console.WriteLine($"{number} => {max}");
+}
+else
+{
+    Console.WriteLine("Условие: целое число из отрезка [10,99]");
+}
