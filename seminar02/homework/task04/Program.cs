0e01a7794bbd93845caba297cb7bb0d46b338910
SmolnyjBuyan <564kot9918@mail.ru>
2024-01-30 16:07:06 +0300

    add seminar02:homework:task04

---
+++ seminar02/homework/task04/Program.cs
@@ -0,0 +1,24 @@
+﻿int N = 4358976;
+
+if (N >= 1)
+{
+    int degree = 0;
+    int temp = N;
+
+    while (temp >= 10)
+    {
+        degree++;
+        temp = temp / 10;
+    }
+
+    while (degree > 0)
+    {
+        Console.Write($"{N / (int)Math.Pow(10, degree) % 10},");
+        degree = degree - 1;
+    }
+    Console.Write($"{N % 10}");
+}
+else
+{
+    Console.WriteLine("Введите натуральное число");
+}
