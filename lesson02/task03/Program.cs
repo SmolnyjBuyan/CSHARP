d49e8ce065acf63d1e3e5d294e624a978a6504ac
SmolnyjBuyan <564kot9918@mail.ru>
2024-01-31 16:48:20 +0300

    add lesson02:task03

---
+++ lesson02/task03/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = { 2, 6, 5, 56, 35, -34, 67, 76, 82, 32 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
