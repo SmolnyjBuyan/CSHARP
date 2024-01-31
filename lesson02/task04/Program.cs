5b9cc461fe9dc4ad143fdb1468ddf136153c0d06
SmolnyjBuyan <564kot9918@mail.ru>
2024-01-31 17:20:42 +0300

    add lesson02:task04

---
+++ lesson02/task04/Program.cs
@@ -0,0 +1,35 @@
+﻿int N = 5;
+int[] arr = { 2, 4, 6, 7, 3 };
+int i = 0;
+int max = arr[0];
+while (i < N)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 0; j < N; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }    
+}
+
+Console.WriteLine(max);
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }      
+}
+
+Console.WriteLine(max);
