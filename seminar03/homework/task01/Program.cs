eb65526dd5684a544095d471044695f1bbca4e80
SmolnyjBuyan <564kot9918@mail.ru>
2024-02-02 10:20:09 +0300

    add seminar03:homework:task01

---
+++ seminar03/homework/task01/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+int count = 0;
+
+foreach (int n in numbers)
+{
+    if (n >= 10 && n <= 90)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
