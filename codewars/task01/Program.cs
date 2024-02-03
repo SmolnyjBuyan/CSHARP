00e3ad42a71c2e822f9028ce52c149dd5ea5921a
SmolnyjBuyan <564kot9918@mail.ru>
2024-02-03 13:20:17 +0300

    Merge home and work directories

---
+++ codewars/task01/Program.cs
@@ -0,0 +1,43 @@
+﻿// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+// Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+int num = 42145;
+string result = num.ToString();
+
+
+int[] answer = result.OrderByDescending(x => x).ToArray();
+
+
+
+
+
+// int num = 42145;
+// string result = string.Concat(num.ToString().OrderByDescending(x => x));
+
+//int answer = Int32.Parse(String.Join("",result));
+// Console.WriteLine(result);
+
+
+// int[] arr = new int[5];
+// int i = 0;
+// while (num > 0)
+// {
+//     arr[i] = num % 10;
+//     num /= 10;
+//     i++;
+// }
+
+// Array.Sort(arr);
+// Array.Reverse(arr);
+
+// foreach (int a in arr)
+// {
+//     Console.Write($"{a} ");
+// }
---
+++ lesson03/task01/Program.cs
@@ -0,0 +1,6 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    return (double)(a * b) / (c + d);
+}
+
+Console.WriteLine(CalculateFormula(1, 2, 3, 4));
