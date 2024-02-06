a5c9325ce4810ff1eb26435eb4d193c23bd6b55e
SmolnyjBuyan <564kot9918@mail.ru>
2024-02-06 16:28:36 +0300

    Add seminar03:homework:task03

---
+++ seminar03/homework/task03/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] array = { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 };
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+
+Console.WriteLine(max - min);
+Console.WriteLine(array.Min());
