fd1ee5c97474abb1d37ec624c7d0191f7916250d
SmolnyjBuyan <564kot9918@mail.ru>
2024-02-07 16:04:28 +0300

    Add lesson04:task03

---
+++ lesson04/task03/Program.cs
@@ -0,0 +1,29 @@
+﻿string GetNumbersFromString(string s)
+{
+    string numbers = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiDigit(e) == true)
+        {
+            numbers = numbers + e;
+        }
+    }
+    return numbers;
+}
+
+int[] CreateArrayFromString(string text)
+{
+    int[] array = new int[text.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = (int)Char.GetNumericValue(text[i]);
+    }
+    return array;
+}
+
+
+string str = Console.ReadLine();
+string numbersFromString = GetNumbersFromString(str);
+int[] result = CreateArrayFromString(numbersFromString);
+
+Console.WriteLine($"[ {string.Join("; ", result)} ]");
