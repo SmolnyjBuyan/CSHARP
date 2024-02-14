d806d19040fe06f2f6df4bae7f05c739e4193bb2
SmolnyjBuyan <564kot9918@mail.ru>
2024-02-14 17:04:39 +0300

    modify task04 by ReverseStringByKotov function

--- seminar05/homework/task04/Program.cs
+++ seminar05/homework/task04/Program.cs
@@ -10,6 +10,72 @@
     return String.Join(" ", words);
 }
 
-string someSentence = "Шла     Саша по шоссе";
-Console.WriteLine(ReverseString(someSentence));
+string ClearExtraSpacesFromStartAndEnd(string str)
+{
+    int i = 0;
+    int j = (str.Length - 1);
+    string result = string.Empty;
 
+    while (str[i] == ' ') i++;
+    while (str[j] == ' ') j--;
+
+    while (i <= j)
+    {
+        result += str[i];
+        i++;
+    }
+    return result;
+}
+
+string ClearExtraSpacesFromCentre(string str)
+{
+    string nerfedStr = ClearExtraSpacesFromStartAndEnd(str);
+    string result = string.Empty;
+    for (int i = 0; i < nerfedStr.Length; i++)
+    {
+        if (nerfedStr[i] == ' ' && nerfedStr[i - 1] == ' ') continue; 
+        result += nerfedStr[i];
+    }
+    return result;
+}
+
+string ReverseStringByKotov(string str)
+{
+    string nerfedStr = ClearExtraSpacesFromCentre(str);
+    int wordsCount = 1;
+    foreach(char symbol in nerfedStr)
+    {
+        if (symbol == ' ') wordsCount++;
+    }
+    string[] words = new string[wordsCount];
+
+    int j = 0;
+    for (int i = 0; i < nerfedStr.Length; i++)
+    {
+        if (nerfedStr[i] == ' ')
+        {
+            j++;
+            continue;
+        }
+        words[j] = words[j] + nerfedStr[i];
+    }
+
+    for (int i = 0; i < (words.Length / 2); i++)
+    {
+        string temp = words[i];
+        words[i] = words[words.Length - 1 - i];
+        words[words.Length - 1 - i] = temp;
+    }
+
+    string result = string.Empty;
+    for (int i = 0; i < words.Length - 1; i++)
+    {
+        result += $"{words[i]} ";
+    }
+    result += words[words.Length - 1];
+    return result;
+}
+
+string someSentence = "        Шла     Саша по            шоссе                ";
+Console.WriteLine(ReverseString(someSentence));
+Console.WriteLine(ReverseStringByKotov(someSentence) + '!');
